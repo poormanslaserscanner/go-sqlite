3b6cd5bafcb78b1ea3ece88c2935d338ae527eac
Markus Bergholz <markuman@gmail.com>
2014-03-21 17:58:25 +0100

    initial release

--- README.md
+++ README.md
@@ -1,4 +1,106 @@
 go-sqlite
 =========
 
-A simple sqlite wrapper for GNU Octave and MATLAB
+A simple sqlite wrapper pure written in Octave and Matlab.
+
+## Examples
+**NOTE:** the output should always be a cell with strings!
+
+Create `table1` in `new.db`  
+
+    >> sqlite('new.db','create table table1 (id INTEGER PRIMARY KEY, Name TEXT, Animal TEXT, Job TEXT)');
+
+Insert values in `table1`
+
+    >> sqlite('new.db','insert into table1 (Name, Animal, Job) values (''Chris'',''Cat'',''Clown'')');
+
+Select everything in `table1`
+
+    >> sqlite('new.db','select * from table1')
+    ans = 1|Chris|Cat|Clown
+
+Create `matrix` in `new.db` with column-name `value` and datatype `REAL`
+
+    >> sqlite('new.db','create table matrix (id INTEGER PRIMARY KEY, value REAL)');
+
+Insert pi into table `matrix`
+
+    >> sqlite('new.db','insert into matrix (value) values (%s)',pi);
+
+Display all tables in `new.db` 
+
+    >> sqlite('new.db','.tables')
+    ans = table1  matrix
+
+List columns in table `matrix`
+
+	>> a=sqlite('new.db','pragma table_info(matrix)')
+	
+	a = 
+	
+    	[1x36 char]
+	
+	>> a{1,1}
+	
+	ans =
+	
+	0|id|INTEGER|0||1
+	1|Value|REAL|0||0
+	Select everything in `table2`
+	
+    >> sqlite('new.db','select * from matrix')
+	ans =
+	{
+	  [1,1] = 1|3.14159265
+
+	}
+    
+
+Get/read variable id1 from `matrix`
+
+
+	>> sqlite('new.db','select Value from matrix where id=1')
+	ans =
+	{
+	  [1,1] = 3.141593
+
+	}
+
+
+## Write a matrix to a database.
+**NOTE:** Input and output is always a 2D double matrix!
+
+go-sqlite helps to write a matrix into a sqlite database. It will create automaticaly a table called `go-sqlite-XX`.  
+The number of rows is saved in id=1. However, it's slow! On Linux, try to save in tmpfs, it's faster _(don't forget to move 
+the file to a more save place then tmps)_  8-)  
+Reading is faster _(even when it's not tmpfs)_!
+
+	>> m=rand(6,7)
+	
+	m =
+	
+	    0.9058    0.5469    0.4854    0.9595    0.7577    0.0318    0.3171
+	    0.1270    0.9575    0.8003    0.6557    0.7431    0.2769    0.9502
+	    0.9134    0.9649    0.1419    0.0357    0.3922    0.0462    0.0344
+	    0.6324    0.1576    0.4218    0.8491    0.6555    0.0971    0.4387
+	    0.0975    0.9706    0.9157    0.9340    0.1712    0.8235    0.3816
+	    0.2785    0.9572    0.7922    0.6787    0.7060    0.6948    0.7655
+	
+	>> sqlite('m.db','save',m)
+	
+	ans =
+	
+	Matrix written to table go-sqlite-1
+	
+	>> n=sqlite('m.db','get','go-sqlite-1')
+	
+	n =
+	
+		0.9058    0.5469    0.4854    0.9595    0.7577    0.0318    0.3171
+ 	   0.1270    0.9575    0.8003    0.6557    0.7431    0.2769    0.9502
+ 	   0.9134    0.9649    0.1419    0.0357    0.3922    0.0462    0.0344
+ 	   0.6324    0.1576    0.4218    0.8491    0.6555    0.0971    0.4387
+ 	   0.0975    0.9706    0.9157    0.9340    0.1712    0.8235    0.3816
+ 	   0.2785    0.9572    0.7922    0.6787    0.7060    0.6948    0.7655
+
+
---
+++ sqlite.m
@@ -0,0 +1,108 @@
+function output_cell = sqlite(varargin)
+
+  switch nargin
+    case {0,1}
+      print_help
+      return
+    case 2
+      dbfile=varargin{1};
+      command=varargin{2};
+      sqlite_check(dbfile, command);
+      output_cell = sqlite_action(dbfile, command);
+    case 3
+      dbfile=varargin{1};
+      command=varargin{2};
+      value=varargin{3};
+      sqlite_check(dbfile, command);
+      if strcmp(command,'save') && ismatrix(value)
+        output_cell = sqlite_save_matrix(dbfile, value);
+      elseif strcmp(command, 'get') && ischar(value)
+        output_cell = sqlite_get_matrix(dbfile, value);
+      else
+        command=sqlite_parse_new_command(command, value);
+        sqlite_action(dbfile, command);
+      end
+      
+    otherwise
+      print_help
+      return
+  end
+
+end
+
+
+function sqlite_check(dbfile, command)
+  [status,~] = system('sqlite3 --version');
+  if (0<status)
+    error('please install sqlite3');
+    return
+  end
+        
+  if ~ischar(dbfile) && ~ischar(command)
+    error('dbfile and command have to be strings')
+    return
+  end
+
+  if ~exist(dbfile, 'file') == 2
+    sprintf('File %s don''t exist and will be created', dbfile)
+  end
+end
+
+function output = sqlite_action(dbfile, command)
+  [status, out] = system(sprintf('sqlite3 %s "%s"', dbfile, command));
+  if status~=0
+    out='ERROR';
+  else
+    output = {out};
+  end
+end
+
+function new_command = sqlite_parse_new_command(command, value);
+  if isnumeric(value)
+    new_command=regexprep(command,'(%s)',['''' num2str(value, '%.8f') '''']);
+  else
+    error('value should be double')
+    return
+  end
+end
+
+function save_name = sqlite_save_matrix(dbfile, matrix);
+  newmatrix=[size(matrix,1); reshape(matrix,[],1)];
+  table_exist=sqlite_action(dbfile,'.tables');
+  for n = 1:99
+    save_name=sprintf('go-sqlite-%d',n);
+    a=strfind (table_exist, save_name);
+    if numel(a{1,1}) == 0
+      break
+    elseif n == 99
+      error('There are alread 99 go-sqlite databases stored')
+      return
+    end
+  end
+  command_create_table=sprintf('create table ''%s'' (id INTEGER PRIMARY KEY, Value REAL)', save_name);
+  sqlite_action(dbfile, command_create_table);
+  for n = 1:size(newmatrix,1)
+    command = sprintf('insert into ''%s'' (Value) values (%s)', save_name, ['''' num2str(newmatrix(n,1), '%.8f') '''']);
+    sqlite_action(dbfile, command);
+  end
+  
+  save_name = ['Matrix written to table  ' save_name];
+end
+
+function matrix = sqlite_get_matrix(dbfile, table);
+  command_get_rows = sprintf('select Value from ''%s'' where id=1', table);
+  r = str2double(sqlite_action(dbfile ,command_get_rows));
+  command_get_allrows = sprintf('SELECT COALESCE(MAX(id)+1, 0) FROM ''%s''', table);
+  n = str2double(sqlite_action(dbfile ,command_get_allrows));
+  matrix=zeros(n-2,1);
+  for k = 2:n-1
+    command_read_value=sprintf('select Value from ''%s'' where id=%d', table, k);
+    matrix(k-1,1)=str2double(sqlite_action(dbfile ,command_read_value));
+  end
+  matrix=reshape(matrix,r,[]);
+end
+
+
+function print_help()
+  fprintf('Write me!\nIn the meantime take a look at https://github.com/markuman/go-sqlite/ :)\n')
+end
