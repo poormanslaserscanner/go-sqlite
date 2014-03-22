e8310073ba70eb5e201ddf665f7cb122fea67082
Markus Bergholz <markuman@gmail.com>
2014-03-22 19:07:26 +0100

    change default columnname from Value to go_sqlite

--- @sqlite/load.m
+++ @sqlite/load.m
@@ -1,6 +1,6 @@
 function out=load(obj, table)
   [~,available_table]=system(sprintf('sqlite3 %s "pragma table_info(%s)"', obj.file, table));
-  known_go_sqlite_default_matrix=[48,124,105,100,124,73,78,84,69,71,69,82,124,48,124,124,49,10,49,124,86,97,108,117,101,124,82,69,65,76,124,48,124,124,48,10];
+  known_go_sqlite_default_matrix=[48,124,105,100,124,73,78,84,69,71,69,82,124,48,124,124,49,10,49,124,103,111,95,115,113,108,105,116,101,124,82,69,65,76,124,48,124,124,48,10];
   if isequal(known_go_sqlite_default_matrix, int8(available_table))
     out = sqlite_get_matrix(obj.file, table);
   else
@@ -10,7 +10,7 @@ function out=load(obj, table)
 end
 
 function matrix = sqlite_get_matrix(dbfile, table);
-  command=sprintf('select Value from "%s"', table);
+  command=sprintf('select go_sqlite from "%s"', table);
   [~,tmatrix]=system(sprintf('sqlite3 %s "%s"', dbfile, command));
   tmatrix=regexp(tmatrix,'[-eE0-9]+.[-eE0-9]+','match');
   tmatrix=str2double (tmatrix)';
--- @sqlite/save.m
+++ @sqlite/save.m
@@ -9,17 +9,17 @@ function save_name = sqlite_save_matrix(dbfile, matrix, save_name);
   reshape_info=size(matrix,1);
   newmatrix=[reshape(matrix,[],1)];
   % create table
-  command_create_table=sprintf('create table "%s" (id INTEGER PRIMARY KEY, Value REAL)', save_name);
+  command_create_table=sprintf('create table "%s" (id INTEGER PRIMARY KEY,  go_sqlite REAL)', save_name);
   system(sprintf('sqlite3 %s "%s"', dbfile, command_create_table));
   % write reshape info to id=1
-  command=sprintf('insert into ''%s'' (Value) values (''%d'')', save_name, reshape_info);
+  command=sprintf('insert into ''%s'' (go_sqlite) values (''%d'')', save_name, reshape_info);
   system(sprintf('sqlite3 %s "%s"', dbfile, command));
   % sqlite is limited at one point, so let's write max. 100 values at ones.
   if size(newmatrix,1) > 100
     for n = 0:(floor(size(newmatrix,1)/100)-1)
       values_string=sprintf('(''%.8f''),',newmatrix((1:100)+(n*100)));
       values_string=values_string(1:end-1);
-      insert_string=sprintf('insert into ''%s'' (Value) values ', save_name);
+      insert_string=sprintf('insert into ''%s'' (go_sqlite) values ', save_name);
       command = [insert_string values_string];
       system(sprintf('sqlite3 %s "%s"', dbfile, command));
     end
@@ -28,7 +28,7 @@ function save_name = sqlite_save_matrix(dbfile, matrix, save_name);
       to=size(newmatrix,1);
       values_string=sprintf('(''%.8f''),',newmatrix(from:to));
       values_string=values_string(1:end-1);
-      insert_string=sprintf('insert into ''%s'' (Value) values ', save_name);
+      insert_string=sprintf('insert into ''%s'' (go_sqlite) values ', save_name);
       command = [insert_string values_string];
       system(sprintf('sqlite3 %s "%s"', dbfile, command));
     end
@@ -36,7 +36,7 @@ function save_name = sqlite_save_matrix(dbfile, matrix, save_name);
   else
     values_string=sprintf('(''%.8f''),',newmatrix);
     values_string=values_string(1:end-1);
-    insert_string=sprintf('insert into ''%s'' (Value) values ', save_name);
+    insert_string=sprintf('insert into ''%s'' (go_sqlite) values ', save_name);
     command = [insert_string values_string];
     system(sprintf('sqlite3 %s "%s"', dbfile, command)); 
   end
