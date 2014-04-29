83909a8cc0494c79df46ef1eac1cdc54ca327c53
Markus Bergholz <markuman@gmail.com>
2014-04-29 20:55:11 +0200

    change sqtest for using obj class and implement max query string length

--- mex/sqtest.m
+++ mex/sqtest.m
@@ -1,6 +1,8 @@
-function out=sqtest(file,query)
-    if ischar(file) && ischar(query)
-        sqlite3(file,query)
+function out=sqtest(obj,query)
+    if ischar(obj.file) && ischar(query)
+	if length(query) <= 2147483647 % default str length limit in sqlite3 ...afaik...
+	        sqlite3(obj.file,query)
+	end
     else
         error('input arguments have to be queries')
     end
