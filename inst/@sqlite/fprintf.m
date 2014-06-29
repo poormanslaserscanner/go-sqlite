96d8282e7088892a7d506136b69329106157224d
Markus Bergholz <markuman@gmail.com>
2014-06-29 14:56:06 +0200

    update fprintf for mex file

--- inst/@sqlite/fprintf.m
+++ inst/@sqlite/fprintf.m
@@ -1,16 +1,15 @@
-function [status,output]=fprintf(obj, string, value)
-  switch nargin
-    case {0,1}
-      print_help
-      return
-    case 2
-      [status,output]=system(sprintf('%s %s "%s"', obj.path, obj.file, string));
-    case 3
-      command=sqlite_parse_new_command(string, value);
-      [status,output]=system(sprintf('%s %s "%s"', obj.path, obj.file, command));
-    otherwise
-      print_help
-      return
+function status = fprintf(obj, string, value)
+    switch nargin
+        case {0,1}
+            print_help
+            return
+        case 2
+            status = sqlite(obj.file, string);
+        case 3
+            status = sqlite(obj.file, string, value);
+        otherwise
+            print_help
+            return
     end
 end
 
