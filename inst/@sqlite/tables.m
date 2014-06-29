b5824419f84d4891349feae71eb28d8cfd172ed1
Markus Bergholz <markuman@gmail.com>
2014-06-29 14:47:41 +0200

    update tables.m for sqlite.mex function. returns now an struct array

--- inst/@sqlite/tables.m
+++ inst/@sqlite/tables.m
@@ -1,17 +1,14 @@
-function [parsed, unparsed]=tables(obj)
+function status=tables(obj)
 
-  switch nargin
-    case {0}
-      print_help
-      return
-    case 1
-      [~,unparsed]=system(sprintf('%s %s ".tables"', obj.path, obj.file));
-      % parse output into a cell...
-      parsed=regexp(unparsed,'(  )+','split'); %% separated with 2xchar(32)
-      parsed=cellfun(@strtrim,parsed,'UniformOutput',false);
-    otherwise
-      print_help
-      return
+    switch nargin
+        case {0}
+            print_help
+            return
+        case 1
+            status = sqlite(obj.file,'SELECT name FROM sqlite_master WHERE type = "table";');
+        otherwise
+            print_help
+            return
     end
 
 end
