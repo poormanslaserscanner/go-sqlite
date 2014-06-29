7eba09cc046ea602e8e64c9eeb1f86a61ba4247e
Markus Bergholz <markuman@gmail.com>
2014-06-29 14:54:11 +0200

    update insert for mex file

--- inst/@sqlite/insert.m
+++ inst/@sqlite/insert.m
@@ -1,13 +1,15 @@
-function status = insert(obj, table, column, data)
+function status = insert(obj, tablename, column, data)
 
-  if ischar(table) && ischar(column)
-    if ~ischar(data)
-      data=num2str(data);
+    if ischar(table) && ischar(column)
+        if isnumeric(data)
+            data=num2str(data);
+        end
+        cct = sprintf('insert into %s (%s) values (''%s'')', tablename, column, data);
+        status = sqlite(obj.file, cct);
+
+    else
+        error('Wrong usage')
+        print_help
     end
-    [status,~] = system(sprintf('%s %s "insert into %s (%s) values (''%s'')"', obj.path, obj.file, table, column, data));
-  else
-    error('Wrong usage')
-    print_help
-  end
 
 end
